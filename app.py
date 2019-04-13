cfa4e6fbaed1b131dea34ecfda3a5ece791d7877
Max Base <MaxBaseCode@Gmail.Com>
2019-04-13 10:03:50 +0430

    Update app.py

--- app.py
+++ app.py
@@ -20,3 +20,37 @@ class flaskLocal(Flask):
 	def process_response(self, response):
 		# response.headers
 		return(response)
+
+app = flaskLocal(__name__)
+
+#app.config.SERVER_NAME = 'asrez.base:80'
+#app.config
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/single')
+def single():
+	return render_template('single.html')
+
+@app.route('/projects/')
+def projects():
+	return 'The project page'
+
+@app.route('/about')
+def about():
+	return 'The about page'
+
+
+# doc_count = col.count_documents(filter, skip=skip)
+# results = col.find(filter).sort(sort).skip(skip).limit(limit)
+
+linkRegistered= [
+	"api",
+	"login",
+	"register",
+	"exit",
+	"signin",
+	"signout",
+]
