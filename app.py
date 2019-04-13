0790866244cb45f4362f66c3694fa13178dc9116
Max Base <MaxBaseCode@Gmail.Com>
2019-04-13 10:02:55 +0430

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-
+from flask import Flask
+from flask import Response
+from flask import render_template
+from flask import Blueprint
+# from flask import request
+from flask import request, jsonify
+import uuid
+import json
+# from app import app, mongo
+from pymongo import MongoClient
+from bson import json_util
+from flask import redirect
