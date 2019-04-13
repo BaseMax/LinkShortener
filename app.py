cdb5a40ceecdb6ec0a140a141f6a89068f7aeb9e
Max Base <MaxBaseCode@Gmail.Com>
2019-04-13 10:04:14 +0430

    Update app.py

--- app.py
+++ app.py
@@ -54,3 +54,59 @@ linkRegistered= [
 	"signin",
 	"signout",
 ]
+
+@app.route('/link/', methods=['GET','POST'], defaults={'identifier':None})
+@app.route('/link/<string:identifier>/',methods=['GET'])
+def link(identifier):
+	mainSite="http://asrez.base/"
+	site="http://asrez.base/link/"
+	if identifier == None or identifier == '':
+		if request.method == "GET":
+			return render_template('link_index.html', error=False, method=request.method, random=uuid.uuid4().hex.upper()[0:7].lower(), site=site, mainSite=mainSite)
+		else:
+			url=request.form.get("url")
+			identifier=request.form.get("identifier")
+			if url and identifier:
+				allow=True
+				for item in linkRegistered:
+					if identifier in item:
+						allow=False
+						break
+				if allow:
+					exists=collectionLink.count_documents({"identifier":identifier})
+					if exists == False:
+						mydict = {"url":url,"identifier":identifier}
+						x = collectionLink.insert_one(mydict)
+						if x.inserted_id:
+							message="Success"
+							status=True
+							return render_template('link_index.html', error=False, message=message, method=request.method, identifier=identifier, status=status, site=site, mainSite=mainSite)
+						else:
+							status=False
+							message="Problem occur in generate link!"
+							return render_template('link_index.html', error=False, message=message, method=request.method, identifier=identifier, status=status, site=site, mainSite=mainSite)
+					else:
+						status=False
+						message="Aleady Exists!"
+						return render_template('link_index.html', error=False, message=message, method=request.method, identifier=identifier, status=status, site=site, mainSite=mainSite)
+				else:
+					status=False
+					message="Identifier is not Allow!"
+					return render_template('link_index.html', error=False, message=message, method=request.method, identifier=identifier, status=status, site=site, mainSite=mainSite)
+			else :
+				status=False
+				message="Failed alert!"
+				return render_template('link_index.html', error=False, message=message, method=request.method, identifier=identifier, status=status, site=site, mainSite=mainSite)
+	elif identifier in linkRegistered:
+		if identifier == "api":
+			return render_template('link_api.html', site=site, mainSite=mainSite)
+		else:
+			return "Soon..."
+	else:
+		get=collectionLink.find_one({"identifier":identifier})
+		if get != None:
+			# return get["url"]
+			return redirect(get["url"], code=302)
+		else:
+			message="Wrong link!"
+			return render_template('link_index.html', error=True, message=message, identifier=identifier, site=site, mainSite=mainSite)
